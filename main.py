8622adaac024dc5cf64414c07a80c63946b9b217
Parker Wahle <52430642+regulad@users.noreply.github.com>
2022-01-27 01:59:47 +0000

    shitheads

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-from typing import List
+from typing import List, Tuple
 
 with open("words.json") as fp:
     wordle_answers: List[str] = json.load(fp)
@@ -11,12 +11,15 @@ letters = input(
 )
 
 if len(letters) == 5:
+    results: List[Tuple[str, int]] = []
     for word in wordle_answers:
         matching_letters: int = 0
         for word_letter, answer_letter in zip(word, letters):
             if word_letter == answer_letter:
                 matching_letters += 1
-        if matching_letters >= 4:
-            print(f"Possible match: {word}")
+        results.append((word, matching_letters))
+    sorted_results: List[Tuple[str, int]] = sorted(results, key=lambda tup: tup[-1], reverse=True)
+    for sorted_result in sorted_results[:10]:
+        print(f"Possible match: {sorted_result[0]} ({sorted_result[-1]}/5)")
 else:
     raise RuntimeError("Wrong word length!")
